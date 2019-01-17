6d67a813e9e62e539a9c97ab6688783e7e4052ae
Derek Cerretani <derekcerretani@gmail.com>
2019-01-17 20:18:54 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -84,3 +84,12 @@ def monopoly_with_second_tier
       :pieces => 4
       } }
 end
+
+def monopoly_with_second_tier
+  monopoly = {
+    :railroads => {
+      :pieces => 4
+      :names => {}
+      }
+    }
+end
